4cf6800d35598343c57cb46ca438c89a361f1724
Maciej Grzeszczak <maciejgrzeszczak@gmail.com>
2017-03-18 11:36:31 +0100

    Adding in kernel.

--- kernel.cu
+++ kernel.cu
@@ -23,6 +23,7 @@
 			FUNCTION DECLARATIONS
 ******************************************/
 __global__ void scanKernel(int *data, const int len, int *out);
+__global__ void addPerBlockKernel(int *data, int *toAdd);
 
 cudaError_t cudaScan(int *arr, const int length, int** out);
 
@@ -70,82 +71,23 @@ __global__ void scanKernel(int *data, const int len, int *out)
 	//data[id] += val;
 }
 
+__global__ void addPerBlockKernel(int *data, int *toAdd)
+{
+	const int threadCount = 1024;
+	const int log_1024 = 10;
+
+	int blockCount = gridDim.x;
+	int tbid = blockIdx.x*threadCount + threadIdx.x;
+	int id = threadIdx.x;
+	int offset = blockIdx.x * threadCount;
+	
+	data[tbid] += toAdd[tbid / threadCount];
+}
+
+
 /* HELPER FUNCTIONS */
 
 /* KERNEL WRAPPING FUNCTIONS */
-cudaError_t cudaScanR(int *data, const int length, int **out) {
-	int *dev_data;
-	cudaError_t cudaStatus;
-
-	*out = (int*)malloc(sizeof(int)*length);
-	if (*out == NULL) ERR("Failed to malloc out cudaScan");
-
-	cudaStatus = cudaSetDevice(0);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-		goto Error;
-	}
-
-	int blockCount = ceil(((float)(length) / MAX_THREADS_PER_BLOCK));
-
-	cudaStatus = cudaMalloc((void**)&dev_data, blockCount * MAX_THREADS_PER_BLOCK * sizeof(int));
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMalloc failed!");
-		goto Error;
-	}
-	cudaMemset((void*)dev_data, 0, blockCount * MAX_THREADS_PER_BLOCK *sizeof(int));
-
-	cudaStatus = cudaMemcpy(dev_data, data, length * sizeof(int), cudaMemcpyHostToDevice);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMemcpy failed!");
-		goto Error;
-	}
-
-	scanKernel << <blockCount, MAX_THREADS_PER_BLOCK >> >(dev_data, length, NULL);
-
-	// Check for any errors launching the kernel
-	cudaStatus = cudaGetLastError();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "scan launch failed: %s\n", cudaGetErrorString(cudaStatus));
-		goto Error;
-	}
-
-	// cudaDeviceSynchronize waits for the kernel to finish, and returns
-	// any errors encountered during the launch.
-	cudaStatus = cudaDeviceSynchronize();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching scan!\n", cudaStatus);
-		goto Error;
-	}
-
-	// Copy output vector from GPU buffer to host memory.
-	cudaStatus = cudaMemcpy(*out, dev_data, length * sizeof(int), cudaMemcpyDeviceToHost);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMemcpy failed!");
-		goto Error;
-	}
-
-	if (blockCount > 1) {
-		int *remaining = (int*)malloc(sizeof(int)*blockCount);
-		if (remaining == NULL) ERR("Failed to allocate memory");
-		for (int i = 0; i < blockCount; i++) {
-			if (i*MAX_THREADS_PER_BLOCK-1 < length)
-				remaining[i] = *out[i * MAX_THREADS_PER_BLOCK - 1] + data[i * MAX_THREADS_PER_BLOCK - 1];
-			else remaining[i] = *out[i*MAX_THREADS_PER_BLOCK - 1] + data[length - 1];
-		}
-		int *scanned;
-		cudaScanR(remaining, blockCount, &scanned);
-
-		for (int i = 0; i < blockCount; i++) {
-			printf("%d ", scanned[i]);
-		}
-	}
-
-Error:
-	cudaFree(dev_data);
-	return cudaStatus;
-}
-
 cudaError_t cudaScan(int *data, const int length, int **out) {
 	int *dev_data;
 	cudaError_t cudaStatus;
@@ -214,11 +156,34 @@ cudaError_t cudaScan(int *data, const int length, int **out) {
 		int *scanned;
 		cudaScan(remaining, blockCount, &scanned);
 
+		int *dev_scanned;
+		cudaStatus = cudaMalloc((void**)&dev_scanned, blockCount * sizeof(int));
+		if (cudaStatus != cudaSuccess) {
+			fprintf(stderr, "cudaMalloc failed!");
+			goto Error;
+		}
+		cudaMemset((void*)dev_data, 0, blockCount *sizeof(int));
 
+		cudaStatus = cudaMemcpy(dev_scanned, scanned, blockCount * sizeof(int), cudaMemcpyHostToDevice);
+		if (cudaStatus != cudaSuccess) {
+			fprintf(stderr, "cudaMemcpy failed!");
+			goto Error;
+		}
+
+		addPerBlockKernel << <blockCount, MAX_THREADS_PER_BLOCK >> >(dev_data,dev_scanned);
+
+		cudaStatus = cudaMemcpy(*out, dev_data, length * sizeof(int), cudaMemcpyDeviceToHost);
+		if (cudaStatus != cudaSuccess) {
+			fprintf(stderr, "cudaMemcpy failed!");
+			goto Error;
+		}
+		cudaFree(dev_scanned);
+
+		/*
 		for (int i = 0; i < length; i++) {
 			(*out)[i] += scanned[i/MAX_THREADS_PER_BLOCK];
 		}
-
+		*/
 
 		free(scanned);
 		free(remaining);
